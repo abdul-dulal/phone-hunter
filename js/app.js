ded1b0d1b8f91bff83d8252e6160fc09515a23cc
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-03-01 19:45:40 +0600

    bonus section add

--- js/app.js
+++ js/app.js
@@ -13,13 +13,13 @@ const searchPhone = () => {
 
 // phone details
 const displayPhone = (phones) => {
-  console.log(phones);
+  // console.log(phones);
   const container = document.getElementById("container");
   phones.forEach((phone) => {
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-    <div class="card">
+    <div class="card mt-4">
       <img src="${phone.image}" class="card-img-top w-75 p-3" alt="..." />
       <div class="card-body">
         <h4 class="card-title">${phone.phone_name}</h4>
@@ -40,6 +40,7 @@ const displayPhone = (phones) => {
 const showDeatils = (details) => {
   //   console.log(deatils);
   const url = `https://openapi.programming-hero.com/api/phone/${details}`;
+  console.log(url);
 
   fetch(url)
     .then((res) => res.json())
@@ -51,22 +52,38 @@ const showMoreDetails = (phoneSlug) => {
   const detailsShow = document.getElementById("detailsShow");
   const div = document.createElement("div");
   div.innerHTML = `
-  <div class="card  w-25 mx-auto">
-  <img src="${phoneSlug.image}" class="card-img-top" alt="..." />
+  <div class="card  w-50 mx-auto">
+  <img src="${
+    phoneSlug.image
+  }"  class="card-img-top w-25 p-3 h-25 mx-auto" alt="..." />
   <div class="card-body">
   <h4 class="card-title">${phoneSlug.name}</h4>
  <p>${phoneSlug.releaseDate ? phoneSlug.releaseDate : "not found"}
     
-  </p>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"> Storage: ${
+  </p> 
+    <h3>Main-Features</h3>
+    <ul class="">
+      <li class=""> <span>Storage : </span>: ${
         phoneSlug.mainFeatures.storage
       }</li>
-      <li class="list-group-item"> DisplaySize:${
+      <li class=""> <span> DisplaySize : </span>:${
         phoneSlug.mainFeatures.displaySize
       }</li>
-      <li class="list-group-item"> ChipSet${phoneSlug.mainFeatures.chipSet}</li>
-      <li class="list-group-item"> Memory${phoneSlug.mainFeatures.memory}</li>
+      <li class=""> <span> ChipSet : </span>${
+        phoneSlug.mainFeatures.chipSet
+      }</li>
+      <li class=""> <span> Memory </span>${phoneSlug.mainFeatures.memory}</li>
+      <li> <span>sensors : </span> ${phoneSlug.mainFeatures.sensors} </li>
+    </ul>
+ 
+    <h3>Others </h3>
+    <ul>
+     <li> <span>Bluetooth: </span> ${phoneSlug.others.Bluetooth} </li>
+     <li> <span>GPS : </span> ${phoneSlug.others.GPS} </li>
+     <li> <span>NFC : </span> ${phoneSlug.others.NFC} </li>
+     <li> <span>Radio : </span> ${phoneSlug.others.Radio} </li>
+     <li> <span>USB : </span> ${phoneSlug.others.USB} </li>
+     <li> <span>WLAN : </span> ${phoneSlug.others.WLAN} </li>
     </ul>
   </div>
 </div>
