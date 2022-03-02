93238903ee6a2bc1cf89b7e14ae07f0954a4dde8
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-03-02 17:58:00 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// style Function
+// notFound and empty  Function
 const empty = (style) => {
   document.getElementById("empty").style.display = style;
 };
@@ -7,20 +7,27 @@ const notFound = (style) => {
   document.getElementById("notFound").style.display = style;
 };
 
+// previous value clear fundtion
+const detail = (id) => {
+  const detailsShow = document.getElementById(id);
+  detailsShow.textContent = "";
+};
+
 // catch  input value
 const searchPhone = () => {
   const inputField = document.getElementById("input-field");
   const inputFieldText = inputField.value;
-
+  // previous container value clear
   const container = document.getElementById("container");
   container.textContent = "";
+  // previous detailsShow value clear
+  detail("detailsShow");
   document.getElementById("spiner").style.display = "block";
   if (inputFieldText == "") {
     document.getElementById("spiner").style.display = "none";
     empty("block");
     notFound("none");
-    const detailsShow = document.getElementById("detailsShow");
-    detailsShow.textContent = "";
+    detail("detailsShow");
   } else {
     // clear input
     inputField.value = "";
@@ -32,12 +39,9 @@ const searchPhone = () => {
           document.getElementById("spiner").style.display = "none";
           notFound("block");
           empty("none");
-          const detailsShow = document.getElementById("detailsShow");
-          detailsShow.textContent = "";
+          detail("detailsShow");
         } else {
           // displayAllPhone(data.data);
-
-          // const result = displayPhoneAll.slice(0, 20);
           displayPhone(data.data.slice(0, 20));
         }
       });
@@ -46,7 +50,6 @@ const searchPhone = () => {
 
 // phone details
 const displayPhone = (phones) => {
-  // console.log(phones);
   const container = document.getElementById("container");
   document.getElementById("spiner").style.display = "none";
   container.textContent = "";
@@ -57,13 +60,14 @@ const displayPhone = (phones) => {
     div.classList.add("col");
     div.innerHTML = `
     <div class="card mt-4">
-      <img src="${phone.image}" class="card-img-top w-75 p-3" alt="..." />
+      <img src="${phone.image}" class="card-img-top w-50 p-3" alt="..." />
       <div class="card-body">
         <h4 class="card-title">${phone.phone_name}</h4>
         <h5 class="card-text">
           ${phone.brand}
         </h5>
         <button class="btn btn-primary rounded mt-3" onclick="showDeatils('${phone.slug}') "> More Deatils</button>
+
       </div>
   </div>
   
@@ -76,55 +80,55 @@ const displayPhone = (phones) => {
 // id releted Details show
 
 const showDeatils = (details) => {
-  //   console.log(deatils);
   const url = `https://openapi.programming-hero.com/api/phone/${details}`;
-  console.log(url);
-
   fetch(url)
     .then((res) => res.json())
     .then((data) => showMoreDetails(data.data));
 };
-
 const showMoreDetails = (phoneSlug) => {
   const detailsShow = document.getElementById("detailsShow");
   detailsShow.textContent = "";
   const div = document.createElement("div");
-
   div.innerHTML = `
   <div class="card  w-50 mx-auto modifi">
   <img src="${
     phoneSlug.image
-  }"  class="card-img-top w-25 p-3 h-25 mx-auto" alt="..." />
+  }"  class="card-img-top w-25 p-3  mx-auto" alt="..." />
   <div class="card-body">
   <h4 class="card-title">${phoneSlug.name}</h4>
- <p>${phoneSlug.releaseDate ? phoneSlug.releaseDate : "not found"}
-    
+ <p>${
+   phoneSlug.releaseDate
+     ? phoneSlug.releaseDate
+     : ` <p class= "text-danger fs-4 text"> ReleaseDate Not Found  </p>`
+ } 
   </p> 
     <h3>Main-Features</h3>
     <ul class="">
-      <li class=""> <span>Storage : </span>: ${
+      <li class=""> <span>Storage : </span> ${
         phoneSlug.mainFeatures.storage
       }</li>
-      <li class=""> <span> DisplaySize : </span>:${
+      <li class=""> <span> DisplaySize : </span>${
         phoneSlug.mainFeatures.displaySize
       }</li>
       <li class=""> <span> ChipSet : </span>${
         phoneSlug.mainFeatures.chipSet
       }</li>
-      <li class=""> <span> Memory </span>${phoneSlug.mainFeatures.memory}</li>
-      <li> <span>sensors : </span> ${phoneSlug.mainFeatures.sensors} </li>
+      <li class=""> <span> Memory : </span>${phoneSlug.mainFeatures.memory}</li>
+       <li> <span>sensors : </span> ${phoneSlug.mainFeatures.sensors} </li>
     </ul>
- 
-    <h3>Others </h3>
-    <ul>
-     <li> <span>Bluetooth: </span> ${phoneSlug.others.Bluetooth} </li>
-     <li> <span>GPS : </span> ${phoneSlug.others.GPS} </li>
-     <li> <span>NFC : </span> ${phoneSlug.others.NFC} </li>
-     <li> <span>Radio : </span> ${phoneSlug.others.Radio} </li>
-     <li> <span>USB : </span> ${phoneSlug.others.USB} </li>
-     <li> <span>WLAN : </span> ${phoneSlug.others.WLAN} </li>
-    </ul>
-    
+    <h3>Others </h3>  
+    ${
+      phoneSlug.others
+        ? ` <ul>
+        <li> <span>Bluetooth: </span> ${phoneSlug.others.Bluetooth} </li>
+       <li> <span>GPS : </span> ${phoneSlug.others.GPS} </li>
+       <li> <span>NFC : </span> ${phoneSlug.others.NFC} </li>
+       <li> <span>Radio : </span> ${phoneSlug.others.Radio} </li>
+       <li> <span>USB : </span> ${phoneSlug.others.USB} </li>
+       <li> <span>WLAN : </span> ${phoneSlug.others.WLAN} </li>
+   </ul>`
+        : ` <p class= "text-danger fs-4 text"> Not Found Infomation😔😔😔  </p>`
+    }
   </div>
   
 </div>
